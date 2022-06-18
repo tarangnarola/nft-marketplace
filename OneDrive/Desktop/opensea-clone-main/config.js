65d8c69846ad61b918d4692ff576067af400ddd4
Tarang Narola <tnarola98@gmail.com>
2022-06-18 09:22:53 +0530

    add loader

---
+++ OneDrive/Desktop/opensea-clone-main/config.js
@@ -0,0 +1,13 @@
+// export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftaddress = "0x87F52C023bA151624401e1965A13776E2125982f";
+export const nftmarketaddress = "0xAf565E2b1093EAB4214037D924Cb46C808c39206";
+//EDIT !!!
+let x = 1;
+//change the above everytime you redeploy your contracts
+
+//command for deploying your contracts on hardhat:
+// npx hardhat run --network localhost scripts/deploy.js
+
+//command for starting or restarting your server:
+//npx hardhat node
---
+++ OneDrive/Desktop/opensea-clone-main/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "imgix",
+    path: '',
+    domains: ['ipfs.infura.io']
+  }
+}
